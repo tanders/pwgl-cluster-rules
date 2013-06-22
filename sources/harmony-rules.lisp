aa8ee0d9eaad24ff40e4329b8c98a451934a3539
Torsten Anders <torsten.anders@beds.ac.uk>
2013-06-23 00:36:19 +0100

    Added rule number-of-sim-PCs and many rules can now constraint multiple voices

--- sources/harmony-rules.lisp
+++ sources/harmony-rules.lisp
@@ -47,7 +47,7 @@
 ;;; only-scale-PCs 
 
 (PWGLDef only-scale-PCs 
-	 ((voice 2)
+	 ((voices 2)
 	  (input-mode  () (ccl::mk-menu-subview :menu-list '(":all" ":beat" ":1st-beat")))
 	  (gracenotes?  () :gracenotes?-include-mbox)
 	  &optional
@@ -57,22 +57,26 @@
 	 "Tones (PC) in the given voice must be a member of the underlying scale (its PCs). The scale is represented as a simultaneous chord in another voice (voice 0 by default). I is either given directly to the clusterengine's pitch domain of that scale voice, or using read-harmony-file, or controlled with other constraints on that voice. 
 
 Args: 
-voice: the voice to which this constraint is applied.
-scale-voice (optional, default 0): the voice representing the underlying scale.
+voices (int or list of ints): the voice(s) to which this constraint is applied.
+
+Optional args:
+scale-voice (int, default 0): the voice representing the underlying scale.
 
 Other arguments are inherited from r-pitch-pitch."
 	 () 
-	 (r-pitch-pitch #'in-harmony?
-			(list voice scale-voice)
-			'(0)
-			input-mode
-			gracenotes?
-			rule-type weight))
+	 (mapcar #'(lambda (voice)
+		     (r-pitch-pitch #'in-harmony?
+				    (list voice scale-voice)
+				    '(0)
+				    input-mode
+				    gracenotes?
+				    rule-type weight))
+	   (if (listp voices) voices (list voices))))
 
 
 ;; only-chord-PCs
 
-(PWGLDef only-chord-PCs ((voice 2)
+(PWGLDef only-chord-PCs ((voices 2)
 			 (input-mode  () (ccl::mk-menu-subview :menu-list '(":beat" ":all" ":1st-beat")))
 			 (gracenotes?  () :gracenotes?-exclude-mbox)
 			 &optional
@@ -82,51 +86,59 @@ Other arguments are inherited from r-pitch-pitch."
 	 "Tones (PC) in the given voice must be a member of the underlying chord (its PCs). The chord is represented as a simultaneous chord in another voice (voice 1 by default). I is either given directly to the clusterengine's pitch domain of that scale voice, or using read-harmony-file, or controlled with other constraints on that voice. 
 
 Args: 
-voice: the voice to which this constraint is applied.
-chord-voice (optional, default 1): the voice representing the underlying chord.
+voices (int or list of ints): the voice(s) to which this constraint is applied.
+
+Optional args:
+chord-voice (int, default 1): the voice representing the underlying chord.
 
 Other arguments are inherited from r-pitch-pitch. For example, it is possible to control whether this constraint should be applied to all notes, or only specific notes (input-mode). By default, it is applied to notes starting on a beat."
 	 () 
-	 (r-pitch-pitch #'in-harmony?
-			(list voice chord-voice)
-			'(0)
-			input-mode
-			gracenotes?
-			rule-type weight))
+	 (mapcar #'(lambda (voice)
+		     (r-pitch-pitch #'in-harmony?
+				    (list voice chord-voice)
+				    '(0)
+				    input-mode
+				    gracenotes?
+				    rule-type weight))
+	   (if (listp voices) voices (list voices))))
 
 
 ;; chord-tone-before/after-rest
 
-(PWGLDef chord-tone-before/after-rest ((voice 2)
+(PWGLDef chord-tone-before/after-rest ((voices 2)
 				       (input-mode  () (ccl::mk-menu-subview :menu-list '(":beat" ":all" ":1st-beat")))
 				       (gracenotes?  () :gracenotes?-exclude-mbox)
 				       &optional
 				       (rule-type  () :rule-type-mbox)
 				       (weight 1)
 				       (chord-voice 1))
-	 "Tones (PC) in the given voice voice after a rest must be a member of the underlying chord PCs.
+	 "Tones (PC) in the given voice(s) after a rest must be a member of the underlying chord PCs.
 
 Args: 
-voice: the voice to which this constraint is applied.
-chord-voice (optional, default 1): the voice representing the underlying chord.
+voices (int or list of ints): the voice(s) to which this constraint is applied.
+
+Optional args:
+chord-voice (int, default 1): the voice representing the underlying chord.
 
 Other arguments are inherited from r-pitch-pitch."
 	 ()
-	 (r-pitch-pitch #'(lambda (pitches1 pitches2)	     
-			    (let* ((voice-pitch1 (first pitches1))
-				   (voice-pitch2 (first pitches2))
-				   (rest1? (null voice-pitch1)) ; 1st note is rest
-				   (rest2? (null voice-pitch2))) ; 2nd note is rest
-			      (cond ((and rest1? rest2?) T) ; in case there are two rests in a row
-				    (rest1? (PC-member voice-pitch2 (second pitches2))) ; after rest
-				    (rest2? (PC-member voice-pitch1 (second pitches1))) 
-				    (T T)))) ; before rest
-			(list voice chord-voice)
-			'(0)
-			input-mode
-			gracenotes?
-			rule-type weight))
-
+	 (mapcar #'(lambda (voice)
+		     (r-pitch-pitch #'(lambda (pitches1 pitches2)	     
+					(let* ((voice-pitch1 (first pitches1))
+					       (voice-pitch2 (first pitches2))
+					       (rest1? (null voice-pitch1)) ; 1st note is rest
+					       (rest2? (null voice-pitch2))) ; 2nd note is rest
+					  (cond ((and rest1? rest2?) T) ; in case there are two rests in a row
+						(rest1? (PC-member voice-pitch2 (second pitches2))) ; after rest
+						(rest2? (PC-member voice-pitch1 (second pitches1))) 
+						(T T)))) ; before rest
+				    (list voice chord-voice)
+				    '(0)
+				    input-mode
+				    gracenotes?
+				    rule-type weight))
+	   (if (listp voices) voices (list voices))))
+		 
 
 ;;;
 ;;; Other pitch related rules
@@ -137,33 +149,35 @@ Other arguments are inherited from r-pitch-pitch."
 ;; NOTE: hack
 (PWGLDef chord-PC-at-1st-tone-HACK 
 	 ((chords ()) 
-	  (voice 2)
+	  (voices 2)
 	  (input-mode  () (ccl::mk-menu-subview :menu-list '(":position-for-pitches" ":index-for-cell")))
 	  &optional
 	  (rule-type  () :rule-type-mbox)
 	  (weight 1)
 	  )
-	 "HACK: The very first tone (PC) in given voice number must be a member of the first of the given chords (the list of list of chords from read-harmony-file).
+	 "HACK: The very first tone (PC) in given voice(s) number must be a member of the first of the given chords (the list of list of chords from read-harmony-file).
 
 NOTE: an index variant for a pitch-pitch constraint (which could access the sim chord PCs) is not available, therefore this workaround."
 	 () 
-	 (r-index-pitches-one-voice #'(lambda (pitch) 
-					(if pitch ; no rest
-					    (member (mod pitch 12)
-						    (mapcar #'(lambda (chord-pitch) (mod chord-pitch 12)) 
-							    (first chords)))
-					  T))
-				    '(0) ; positions
-				    voice
-				    input-mode
-				    rule-type weight))
-
+	 (mapcar #'(lambda (voice)
+		     (r-index-pitches-one-voice #'(lambda (pitch) 
+						    (if pitch ; no rest
+							(member (mod pitch 12)
+								(mapcar #'(lambda (chord-pitch) (mod chord-pitch 12)) 
+									(first chords)))
+						      T))
+						'(0) ; positions
+						voice
+						input-mode
+						rule-type weight))
+	   (if (listp voices) voices (list voices))))
+		 
 
 ;; stepwise-non-chord-tone-resolution
 
 (PWGLDef stepwise-non-chord-tone-resolution 
 	 ((step-size 2) 
-	  (voice 2)
+	  (voices 2)
 	  (input-mode  () (ccl::mk-menu-subview :menu-list '(":all" ":beat" ":1st-beat")))
 	  (gracenotes?  () :gracenotes?-exclude-mbox)
 	  &optional
@@ -173,37 +187,41 @@ NOTE: an index variant for a pitch-pitch constraint (which could access the sim 
 	 "Every tone (PC) that is not a chord tone (member of sim chord PCs, in voice 1 by default) is reached/left by a step of the given step size.
 
 Args: 
-voice: the voice to which this constraint is applied.
-scale-voice (optional): the voice representing the underlying chord.
+voices (int or list of ints): the voice(s) to which this constraint is applied.
+
+Optional args:
+chord-voice (int, default 1): the voice representing the underlying chord.
 
 Other arguments are inherited from r-pitch-pitch."
 	 () 
-	 (r-pitch-pitch #'(lambda (pitches1 pitches2 pitches3)
-			    ;; Every pitchesN is a list of the form (chord-pitches voice-pitch) 
-			    (if (and (first pitches2) (second pitches2))  ;; no rests
-				(let ((voice-pitch2 (second pitches2)))
-				  (if (not (member (mod voice-pitch2 12) ; middle PC
-						   ;; chord PCs
-						   (mapcar #'(lambda (p) (mod p 12)) (first pitches2))))
-				      (and (if (second pitches1) ; no rest
-					       (<= (abs (- (second pitches1) voice-pitch2)) step-size)
-					     T)
-					   (if (second pitches3) ; no rest
-					       (<= (abs (- voice-pitch2 (second pitches3))) step-size)
-					     T))
-				    T))
-			      T))
-			(list chord-voice voice)
-			'(0)
-			input-mode
-			gracenotes?
-			rule-type weight))
+	 (mapcar #'(lambda (voice)
+		     (r-pitch-pitch #'(lambda (pitches1 pitches2 pitches3)
+					;; Every pitchesN is a list of the form (chord-pitches voice-pitch) 
+					(if (and (first pitches2) (second pitches2))  ;; no rests
+					    (let ((voice-pitch2 (second pitches2)))
+					      (if (not (member (mod voice-pitch2 12) ; middle PC
+							       ;; chord PCs
+							       (mapcar #'(lambda (p) (mod p 12)) (first pitches2))))
+						  (and (if (second pitches1) ; no rest
+							   (<= (abs (- (second pitches1) voice-pitch2)) step-size)
+							 T)
+						       (if (second pitches3) ; no rest
+							   (<= (abs (- voice-pitch2 (second pitches3))) step-size)
+							 T))
+						T))
+					  T))
+				    (list chord-voice voice)
+				    '(0)
+				    input-mode
+				    gracenotes?
+				    rule-type weight))
+	   (if (listp voices) voices (list voices))))
 
 
 
 ;;; unequal-sim-PCs
 
-(PWGLDef unequal-sim-PCs 
+(PWGLDef unequal-sim-PCs-aux 
 	 ((voices 0)
 	  (input-mode  () (ccl::mk-menu-subview :menu-list '(":all" ":beat" ":1st-beat" ":1st-voice")))
 	  (gracenotes?  () :gracenotes?-include-mbox)
@@ -224,16 +242,19 @@ Other arguments are inherited from r-pitch-pitch."
 			gracenotes?
 			rule-type weight))
 
-(PWGLDef unequal-sim-PCs-poly
+(PWGLDef unequal-sim-PCs
 	 ((voices 0)
 	  (input-mode  () (ccl::mk-menu-subview :menu-list '(":all" ":beat" ":1st-beat" ":1st-voice")))
 	  (gracenotes?  () :gracenotes?-include-mbox)
 	  &optional
 	  (rule-type  () :rule-type-mbox)
 	  (weight 1))
-	 "Sim PCs in all given voices (ints in ascending order) are unequal to each other..
+	 "Sim PCs in all given voices (ints in ascending order) are unequal to each other.
 
-Arguments are inherited from r-pitch-pitch."
+Arguments are inherited from r-pitch-pitch.
+
+TODO: Revise this definition -- can the interplay with unequal-sim-PCs-aux be simplified?
+"
 	 () 
 	 (let ((len (length voices))
 	       (rev (reverse voices)))
@@ -245,8 +266,46 @@ Arguments are inherited from r-pitch-pitch."
 		   (arithm-ser 0 1 (1- len)))))
 
 
-;; TODO: 
-;; - generalise for more voices 
+;; TODO:
+;; - !! Automatically reduce PC-number internally, if the number of sim pitches is snmaller due to rests (some pitch = NIL)
+;; - ? Add additional input modes that not only take harmonic slices but all tones within a certain time frame into account, namely a full beat duration, a full bar duration, the beginning of a new harmony, and the whole duration of a harmony -- likely the current implementation of r-pitch-pitch does not allow for these cases.
+;; - ? Improve efficiency: shall I internally also call r-pitch-pitch with subsets of voices and with a reduced PC-number? That will help the solver, but that possibly also restricts the range of possible solutions.
+;; - ?? Have PC-number controlled with BPF? Possibly the current implementation of r-pitch-pitch does not allow for that. Also, that would restrict the would only work if underlying harmony allows for that, but could be useful 
+(PWGLDef number-of-sim-PCs ((PC-number 2)
+			    (condition () (ccl::mk-menu-subview :menu-list '(":min" ":equal" ":max")))
+			    (voices '(0 1))
+			    (timepoints '(0))
+			    (input-mode  () (ccl::mk-menu-subview :menu-list '(":all" ":beat" ":1st-beat" ":1st-voice" ":at-timepoints")))
+			    (gracenotes? () (ccl::mk-menu-subview :menu-list '(":exclude-gracenotes" ":include-gracenotes")))
+			    &optional
+			    (rule-type () (ccl::mk-menu-subview :menu-list '(":true/false" ":heur-switch")))
+			    (weight 1))
+	 "Controls the number of simultaneous pitch classes. Useful, for example, to require that some underlying harmony is fully expressed.
+
+Args: 
+PC-number: the number of the simultaneous PCs. The meaning of this setting depends on the argument condition.
+condition: Whether the number of simultaneous pitch classes should be at least the given PC-number (:min), or exactly that number (:equal), or at most that number (:max). 
+voices: the list of voices to which the rule is applied.
+
+Other arguments are inherited from r-pitch-pitch."
+	 () 
+	 (r-pitch-pitch #'(lambda (pitches)
+			    (let ((harm (remove-duplicates (pw::g-mod pitches 12))))
+			      (if harm				  
+			        (funcall (case condition
+					   (:min #'>=)
+					   (:equal #'=)
+					   (:max #'<=))
+				   (length harm) PC-number)
+			      t)))
+			voices
+			timepoints
+			input-mode
+			gracenotes?
+			rule-type weight))
+
+
+
 (PWGLDef no-empty-sim-consonances-2parts 
 	 ((voices 0)
 	  (timepoints '(0))
@@ -256,7 +315,9 @@ Arguments are inherited from r-pitch-pitch."
 	  (rule-type  10 (ccl::mk-menu-subview :menu-list '(":true/false" ":heur-switch")))
 	  (weight 1))
 	 "Disallows a perfect consonance between sim notes -- simple version for two parts.
-All arguments are inherited from r-pitch-pitch."
+All arguments are inherited from r-pitch-pitch.
+
+TODO: generalise for more voices."
 	 () 
 	 (r-pitch-pitch #'(lambda (pitches)
 			    (if (first pitches) ; no rests 
@@ -267,8 +328,7 @@ All arguments are inherited from r-pitch-pitch."
 			timepoints
 			input-mode
 			gracenotes?
-			rule-type
-			weight))
+			rule-type weight))
 
 ;;
 ;; Tintinnabuli rules 
@@ -285,24 +345,24 @@ Ideas for using tintinnabuli in settings beyond the "Arvo Pärt sound". Doing th
    - M-voice and T-voice in different beat subdivisions or otherwise clearly separate
 |#
 
-(PWGLDef tintinnabuli-M-voice ((voice 0)
+(PWGLDef tintinnabuli-M-voice ((voices 0)
 			       &optional
 			       (max-interval 2)
 			       (scale-voice 0))
 	 "Rules for a tintinnabuli M-voice, inspired by Arvo Pärt (slightly generalised, because this rule is applicable over any harmony). The voice consists only of scale tones that move stepwise (max interval is whole tone).  
 
 Args: 
-voice: the number of the voice to constrain.
+voices (int or list of ints): the number of the voice(s) to constrain.
+
 Optional:
 max-interval (default 2): maximum interval in semitones.
 scale-voice (default 0): the voice representing the underlying scale."
 	 () 
 	 (rules->cluster 
-	  (min/max-interval voice :max-interval max-interval)
-	  (only-scale-PCs voice :all :include-gracenotes
-			  :true/false 1 scale-voice)))
+	  (min/max-interval voices :max-interval max-interval)
+	  (only-scale-PCs voices :all :include-gracenotes :true/false 1 scale-voice)))
 
-(PWGLDef tintinnabuli-T-voice ((voice 0)
+(PWGLDef tintinnabuli-T-voice ((voices 0)
 			       &key			       
 			       (min-interval 3)
 			       (max-interval 12)
@@ -310,16 +370,16 @@ scale-voice (default 0): the voice representing the underlying scale."
 	 "Rules for a tintinnabuli T-voice, inspired by Arvo Pärt (slightly generalised, because this rule is applicable over any harmony). The voice consists only of chord tones, and the minimum/maximum interval size can be controlled.  
 
 Args: 
-voice: the number of the voice to constrain.
+voices (int or list of ints): the number of the voice(s) to constrain.
+
 Key args:
 min-interval (default 3): minimum interval in semitones.
 max-interval (default 12): maximum interval in semitones.
 chord-voice (default 1): the voice representing the underlying chord."
 	 () 
 	 (rules->cluster 
-	  (min/max-interval voice :min-interval min-interval :max-interval max-interval)
-	  (only-chord-PCs voice :all :include-gracenotes
-			  :true/false 1 chord-voice)))
+	  (min/max-interval voices :min-interval min-interval :max-interval max-interval)
+	  (only-chord-PCs voices :all :include-gracenotes :true/false 1 chord-voice)))
 
 
 
--- sources/menus.lisp
+++ sources/menus.lisp
@@ -15,10 +15,10 @@
 		 accent-is-longer-than-predecessor))
      ("melody"  (no-direct-repetition no-repetition min/max-interval durations-control-intervals))
      ("harmony"  (only-scale-PCs only-chord-PCs chord-tone-before/after-rest
-		  chord-PC-at-1st-tone-HACK stepwise-non-chord-tone-resolution unequal-sim-PCs unequal-sim-PCs-poly
-		  no-empty-sim-consonances-2parts
+		  chord-PC-at-1st-tone-HACK stepwise-non-chord-tone-resolution 
+		  unequal-sim-PCs number-of-sim-PCs no-empty-sim-consonances-2parts
 		  tintinnabuli-M-voice tintinnabuli-T-voice))
-     ("counterpoint"  (no-voice-crossing no-voice-crossing-poly))
+     ("counterpoint"  (no-voice-crossing))
      ("score" (voice->durations voice->pitches))
      ("utilities"  (file-in-this-directory read-lisp-file read-harmony-file))
      ))))
