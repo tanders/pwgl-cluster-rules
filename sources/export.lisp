6afff8ed4a6971a29b00dd92bb73b2ac18de5caf
Torsten Anders <torsten.anders@beds.ac.uk>
2013-07-12 00:44:34 +0200

    Added exports for debugging

---
+++ sources/export.lisp
@@ -0,0 +1,7 @@
+(in-package :cluster-rules)
+
+;; ;; TMP: for getting some patch running again
+;; (defun accent-is-longer-than-predecessor (&rest args)
+;;   'bal)
+;; (export '(accent-is-longer-than-predecessor)
+;; 	:cluster-rules)
