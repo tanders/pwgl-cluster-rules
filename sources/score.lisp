4780e3d6208775efbf179bfbfbb6fd48933ddb7e
Torsten Anders <torsten.anders@beds.ac.uk>
2013-07-14 22:54:20 +0200

    Greatly refined all defs in this file of score accessors, e.g., transformation into start times added, ties now handled, returning ratios supported etc.

--- sources/score.lisp
+++ sources/score.lisp
@@ -1,32 +1,60 @@
 (in-package :cluster-rules)
 
+(PWGLDef voice->start-times ((voice nil) &key (rationalize? NIL))
+	 "Expects a voice and returns a list of its note and rest start times. Start times of rests are notated as negative numbers. If a score or part is given instead, then the first voice is selected.
 
+NOTE: The resulting start times depend on the tempo of the score. If a quarternote should result in a 0.25 (or 1/4) then the tempo of the score should be constant at 60.
 
-;; This is the starting point for defining heuristic rules that vary somehow an existing score
-;;
-;; NOTE: results also depend on tempo!
-(PWGLDef voice->durations ((score nil))
-	 "Expects a voice and returns a list of its note durations. If a score or part is given instead, then the first voice is selected.
-
-TODO:
-- integrate (ccl::tied-p chord)
-
-Current limitations:
-- Ties are not recognised and ignored.
-- Note durations extracted from floats, so the precision is limited."
+Args:
+rationalize? (Boolean): If the score has a constant tempo of 60, then start times can be output as ratios, by setting this argument to T.
+"
 	 ()
-	 (pw:x->dx
-	  (mapcar #'(lambda (chord) 
-		      (* (ccl::start-time chord)
-			 (if (ccl::rest-p chord) -1 1)))
-		  (ccl::collect-enp-objects (first (ccl::collect-enp-objects score :voice)) 
-					    :chord))))
+	 (mappend #'(lambda (c) 
+		      (if (ccl::tied-p c)
+			  NIL
+			(list (let ((start-time
+				     ;; divide by four - transform beats for dur 1 into 1/4 
+				     (/ (* (ccl::start-time c)
+					   (if (ccl::rest-p c) -1 1))
+					4)))
+				(if rationalize? 
+				    (rationalize start-time)
+				  start-time)))))
+		  (ccl::collect-enp-objects (first (ccl::collect-enp-objects voice :voice)) 
+					    :chord)))
 
 
-(PWGLDef voice->pitches ((score nil))
-	 "Expects a voice and returns a list of its note pitches. If a score or part is given instead, then the first voice is selected."
+(PWGLDef voice->durations ((voice nil) &key (rationalize? NIL))
+	 "Expects a voice and returns a list of its note and rest durations. Durations of rests are notated as negative numbers. If a score or part is given instead, then the first voice is selected.
+
+NOTE: The resulting start times depend on the tempo of the score. If a quarternote should result in a 0.25 (or 1/4) then the tempo of the score should be constant at 60.
+
+Args:
+rationalize? (Boolean): If the score has a constant tempo of 60, then durations can be output as ratios (i.e., 1/4 means a quarter note), by setting this argument to T."
 	 ()
-	 (mapcar #'(lambda (chord) 
-		     (ccl::midi (first (ccl::collect-enp-objects chord :note))))
-		 (ccl::collect-enp-objects (first (ccl::collect-enp-objects score :voice)) 
-					   :chord)))
+	 (let ((starts (voice->start-times voice)))
+	   (mapcar #'(lambda (start dur) 
+		       (if rationalize? 
+			   (rationalize (* (signum start) dur))
+			   (* (signum start) dur)))
+		   starts
+		   (pw:x->dx
+		    (append (mapcar #'abs starts)
+			    (let ((c (first (last (ccl::collect-enp-objects 
+						   (first (ccl::collect-enp-objects voice :voice))
+						   :chord)))))
+			      (if (ccl::tied-p c)
+				  NIL
+				(list (/ (+ (ccl::start-time c) (ccl::duration c)) 4)))))))))
+
+
+(PWGLDef voice->pitches ((voice nil))
+	 "Expects a voice and returns a list of its note pitches. If a score or part is given instead, then the first voice is selected. In case there is a rest, NIL is returned.
+"
+	 ()
+	 (mappend #'(lambda (c) 
+		      (cond ((ccl::rest-p c) '(NIL))
+			    ((ccl::tied-p c) NIL)
+			    (T (list (ccl::midi (first (ccl::collect-enp-objects c :note)))))))
+		  (ccl::collect-enp-objects (first (ccl::collect-enp-objects voice :voice)) 
+					    :chord)))
