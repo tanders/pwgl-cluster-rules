2773c671a33ae6168ec37272b878b5fc1cb7a754
Torsten Anders <torsten.anders@beds.ac.uk>
2013-07-14 12:09:03 +0200

    Added all applied rules already available in cluster engine to menus

--- sources/menus.lisp
+++ sources/menus.lisp
@@ -10,22 +10,27 @@
 		 compose-functions)
 		 ("mappings" (mp-add-offset mp-multiply mp-add-random-offset))
 		 ("transformations" (trfm-scale trfm-add-BPF trfm-multiply-BPF trfm-reverse)))
-     ("rhythm"  (no-two-consecutive-syncopations no-syncopation only-simple-syncopations only-simple-tuplet-offs
+     ("rhythm"  ("Cluster Engine" (cluster-engine::r-metric-hierarchy
+				   cluster-engine::r-rhythm-hierarchy))
+                (no-two-consecutive-syncopations no-syncopation only-simple-syncopations only-simple-tuplet-offs
 		 start-with-rest
 		 ; accent-is-longer-than-predecessor
 		 metric-accents
 		 accents-in-other-voice)
                  ("accent rules" (mk-accent-has-at-least-duration-ar)))
-     ("melody"  (min/max-interval set-intervals accumulative-interval
+     ("melody"  ("Cluster Engine" (cluster-engine::r-mel-interval-one-voice))
+                (min/max-interval set-pitches set-intervals accumulative-interval
 		 no-direct-repetition no-repetition 
 		 restrict-consecutive-directions resolve-skips
 		 durations-control-intervals
 		 ))
-     ("harmony"  (only-scale-PCs only-chord-PCs chord-tone-before/after-rest
+     ("harmony"  ("Cluster Engine" (cluster-engine::r-chords))
+                 (only-scale-PCs only-chord-PCs chord-tone-before/after-rest
 		  chord-PC-at-1st-tone-HACK stepwise-non-chord-tone-resolution 
 		  unequal-sim-PCs number-of-sim-PCs set-harmonic-intervals
 		  tintinnabuli-M-voice tintinnabuli-T-voice))
-     ("counterpoint"  (no-voice-crossing no-parallels))
+     ("counterpoint" ("Cluster Engine" (cluster-engine::r-canon))
+                     (no-voice-crossing no-parallels))
      ("score" (voice->durations voice->pitches))
      ("utilities"  (file-in-this-directory read-lisp-file read-harmony-file 
 		    output-filename))
